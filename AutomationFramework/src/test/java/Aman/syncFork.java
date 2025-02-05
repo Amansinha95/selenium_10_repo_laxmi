22c71b30d47bb432a833932fd030437371b32ea5
Rajlaxmi <rajlaxmia23@gmail.com>
2025-02-05 10:21:36 +0530

    sync

---
+++ AutomationFramework/src/test/java/Aman/syncFork.java
@@ -0,0 +1,11 @@
+package Aman;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class syncFork {
+@Test
+public void sync() {
+	Reporter.log("Sync class successfully added", true);
+}
+}
